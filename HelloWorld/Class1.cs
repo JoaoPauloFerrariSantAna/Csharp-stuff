3dd40bb53fbf597c6ac83163720d12bec28c01aa
JoaoPauloFerrariSantAna <94869217+JoaoPauloFerrariSantAna@users.noreply.github.com>
2024-04-27 09:04:31 -0300

    Adicionando pasta inicial (Hello World)
    Sábado passado estava ocorrendo um treinamento do qual eu estava participando...
    Eu não consegui mandar os arquivos, na hora. Mas agora eu estou mandando

---
+++ HelloWorld/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+// 1 - Você precisa usar o namespace para usar a classe em outros arquivos
+namespace AulaCsharp;
+
+public class Teste
+{
+	public Teste()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public bool is_par(int num)
+    {
+        return (num % 2 == 0);
+    }
+
+	public void show_int_list(List<int> list)
+	{
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.WriteLine(list[i]);
+        }
+    }
+
+    public void show_str_list(List<string> list)
+    {
+        for(int i = 0; i < list.Count; i++)
+        {
+            Console.WriteLine(list[i]);
+        }
+    }
+
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿// 2 - É assim que se importa um namespace
+// Você sempre vai querer
+using AulaCsharp;
+
+// "List" é como se fosse um genérico de Java 
+// mude o "int" para qualquer tipo nos dois "<>" para alterar o tipo da lista
+// para adicionar coisa use ".Add(<alguma coisa>)" ou inicie a "List" com os "{}"
+List<int> lista_num = new List<int> { 413, 612, 111111 };
+List<string> lista_str = new List<string> { "Hello", "World", "Banana" };
+
+Teste teste = new Teste();
+
+teste.show_int_list(lista_num);
+teste.show_str_list(lista_str);
+
+int numero1 = 2;
+int numero2 = 7;
+int soma = numero1 + numero2;
+
+bool soma_maior_dez = (soma > 10);
+bool e_multiplo = (numero2 % numero1 == 0);
+
