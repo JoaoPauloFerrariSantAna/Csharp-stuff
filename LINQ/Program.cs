2b4c4557e4f1e3bf1878c2e1a0cdaea86147e85b
JoaoPauloFerrariSantAna <94869217+JoaoPauloFerrariSantAna@users.noreply.github.com>
2024-04-27 09:11:38 -0300

    Adicionando arquivos relacionados ao LINQ csharp
    (aqui eu vou precisar dar uma arrumada)

---
+++ LINQ/Program.cs
@@ -0,0 +1,19 @@
+﻿// LINQ Significa: language integrated query
+
+List<string> list = new List<string> { "banana", "maça", "Feijoada" };
+
+string user_input = Console.ReadLine();
+
+// isso é um lambda function
+var lambda = () => Console.WriteLine("hello from lambda");
+
+List<string> result = list.Where(what_to_search => 
+    what_to_search.Contains(user_input))
+    .ToList();
+
+var result2 = from x in list
+              where x.Contains(user_input)
+              orderby x ascending
+              select x;
+
+Console.WriteLine(result2);
